5fd0aa942bb803678dc32e1cea829efaddaf93be
Saad <saadabouelsooud@gmail.com>
2025-04-14 14:02:25 +0200

    set locale

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'router/app_router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -16,6 +18,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Tab Navigation',
+      locale: const Locale('ar'), // Set Arabic locale
+      supportedLocales: const [
+        // Locale('en'),
+        Locale('ar'), // Add more if needed
+      ],
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
